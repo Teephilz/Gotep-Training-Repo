d176e9f1e5f894d8e1d0e080066a40f888dfe724
taiwo <oyetaiwophilip@gmail.com>
2025-07-31 19:22:30 +0100

    new Changes

--- users-website/src/components/Header.jsx
+++ users-website/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import ThemeContext from '../context/ThemeContext';
 import { useContext } from 'react';
+import { HiOutlineSun } from 'react-icons/hi';
 
 const Header = () => {
 
@@ -8,11 +9,11 @@ const Header = () => {
 
   return (
      <div className="flex flex-col  pb-10  fixed top-0 w-full">
-    <header className="p-4 flex justify-between items-center bg-white 
-     border-[#613613]  shadow-[#613613] shadow-[-5px_5px_10px_rgba(0,0,0,0.3)] ">
-      <Link to="/" className="font-bold">PostApp</Link>
-      <button onClick={toggleTheme}>
-        {darkMode ? 'Light Mode' : 'Dark Mode'}
+    <header className="p-4 flex justify-between items-center bg-slate-950
+     border-[#613613]  shadow-[#613613] shadow-[-2px_2px_10px_rgba(0,0,0,0.3)] ">
+      <Link to="/" className="font-bold text-white">PostApp</Link>
+      <button className =" dark:text-black"onClick={toggleTheme}>
+        <HiOutlineSun size={30} color={"white"}/>
       </button>
     </header>
  </div>
